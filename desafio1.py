c9996f13ff31caf91a38745794df1690f14c7b13
Gustavo-Uliana <gusuliana11@gmail.com>
2024-08-28 19:20:51 -0300

    first commit

---
+++ desafio1.py
@@ -0,0 +1,29 @@
+# Quadrado
+
+altura_quad = 20
+
+comprimento_quad = 20
+
+area_quadrado = (altura_quad * comprimento_quad)
+
+print(area_quadrado)
+
+# Retângulo
+
+altura_ret = 20
+
+comprimento_ret = 40
+
+area_ret = (altura_ret * comprimento_ret)
+
+print(area_ret)
+
+# Triângulo
+
+altura1_tri = 10
+
+altura2_tri = 10
+
+area_tri = (altura1_tri * altura2_tri)
+
+print(area_tri)
